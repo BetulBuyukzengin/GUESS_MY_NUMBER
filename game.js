89fa355cf178026948cf3b81d8418296917a7405
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2023-10-27 01:05:04 +0300

    Oyun kaybedildiğinde gelen video hatası düzeltildi

--- game.js
+++ game.js
@@ -14,6 +14,7 @@ export const guessInput = document.querySelector(".guess");
 export const checkBtn = document.querySelector(".check");
 
 export class Game {
+  guess;
   gameIsOver = false;
   html;
   #score = 20; //starter score
@@ -64,14 +65,16 @@ export class Game {
   }
   //Data control
   dataIsValid() {
-    const guess = Number(document.querySelector(".guess").value);
-    if (!guess) {
+     this.guess = Number(document.querySelector(".guess").value);
+    if (!this.guess) {
       this.displayMessage("Not a number!");
-    } else if (this.#secretNumber === guess) {
+    } else if (this.#secretNumber === this.guess) {
       this.gameIsOver = true;
       againBtn.disabled = false;
+
       document.querySelector(".number").textContent = this.#secretNumber; //  random number show in box
       this.displayMessage("Correct Number!");
+
       beforeWin.classList.add("hidden");
       this.html = `
        <video autoplay loop>
@@ -86,43 +89,58 @@ export class Game {
       }
       checkBtn.disabled = true;
       guessInput.disabled=true;
-    } else if (this.#secretNumber !== guess) {
+    } 
+    else if (this.#secretNumber !== this.guess) {
       if (this.#score > 1) {
         this.displayMessage(
-          guess > this.#secretNumber ? "Too high! " : "Too low"
+          this.guess > this.#secretNumber ? "Too high! " : "Too low"
         );
         this.#score--;
         document.querySelector(".score").textContent = this.#score;
         document.querySelector(".guess").value = "";
       } else {
         againBtn.disabled = false;
+
         this.displayMessage("You lost the game!");
         document.querySelector(".score").textContent = 0;
         document.querySelector("input").disabled = true; // blocks input
+        msgText.classList.add('lostGame');
         this.gameIsOver = true;
         guessInput.disabled = true;
+        main.style.opacity = 0.7;
       }
     }
   }
+
+  // Win video remove feature
+  removeFirstElement(){
+    const data = document.querySelector("#beforeWin").closest(".main");
+    data.firstElementChild.style.display = "none";
+  }
+
   // Reset game onclick again button
   again() {
     document.querySelector(".again").addEventListener("click", () => {
+      if(this.#secretNumber === this.guess) this.removeFirstElement()
+
       this.#secretNumber = Math.trunc(Math.random() * 20) + 1;
       this.#score = 20;
+
       beforeWin.classList.remove("hidden");
-      const data = document.querySelector("#beforeWin").closest(".main");
-      data.firstElementChild.style.display = "none";
+      
       document.querySelector(".score").textContent = this.#score;
       this.displayMessage("Start guessing...");
       msgText.classList.remove("messageAnimation");
+
       document.querySelector(".number").textContent = "?";
-      document.querySelector(".number").style.width = "15rem";
       document.querySelector(".guess").value = "";
       document.querySelector("input").disabled = false;
+
       this.gameIsOver = false;
       checkBtn.disabled = false;
       guessInput.disabled=false;
-
+      main.style.opacity = 1;
+      msgText.classList.remove('lostGame');
     });
   }
   // Check data on keydown and click
